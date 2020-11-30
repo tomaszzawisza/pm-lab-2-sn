47aa25d09c83d0913dcc9ed67c30d99ac6d2c32e
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2020-11-30 09:21:58 +0100

    Wykonano zadanie 2.2.1–pojedyncza dioda LED D13

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>
+#include <avr/io.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+int main()
+{
+  uint32_t i;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  DDRB |= (1 << 5);
+  while (1)
+  {
+    PORTB |= (1 << 5);
+    i = 0x3FFFF;
+    do
+    {
+      asm volatile("nop");
+    } while (i--);
+    PORTB &= !(1<<5);
+    i = 0x3FFFF;
+    do
+    {
+      asm volatile("nop");
+    } while (i--);
+
+  }
 }
